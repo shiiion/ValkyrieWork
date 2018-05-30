de33fe19a79b7684f6890c865dba6dc826b636b5
Unknown <genoclysm@outlook.com>
2018-05-30 15:01:02 -0400

    had to rewrite aimbot header
    didnt save it and committed 1 line lol

--- ValkyrieWork/aimbotfeatures.cpp
+++ ValkyrieWork/aimbotfeatures.cpp
@@ -10,51 +10,102 @@
 
 namespace valkyrie
 {
+	//legitbot implementation
 	auto LegitBot::execFeature() const -> void
 	{
 		CSPlayer& localPlayer = playerList.getLocalPlayer();
-
+		
 		float bestEnemyDistance;
-		uint32_t targetIndex = chooseEnemy(bestEnemyDistance);
+		const uint32_t targetIndex = chooseEnemy(bestEnemyDistance, this->friendlyFire);
+
+		const HitboxID boneID = chooseBone(targetIndex);
+
+		//double check the target index values aren't bs
+		if (targetIndex < 0 || unsigned(targetIndex) >= playerList.size())
+		{
+			return;
+		}
+		const CSPlayer& targetPlayer = playerList[targetIndex];
+		const vec3 targetBone = targetPlayer.bones[boneID];
+
+		if (!localPlayer.isDead)
+		{
+			
+		}
+
+
 
 	}
 
+	auto LegitBot::chooseBone(const uint32_t bestTarget) const -> HitboxID
+	{
+		HitboxID bestBone = midTorso;
+		const vec2 screenCenter = vec2(globals.screenWidth / 2.f, globals.screenHeight / 2.f);
+		float minDist = std::numeric_limits<float>::max();
+		if (bestTarget >= 0)
+		{
+			const vector<HitboxID> hitboxes = { head,neck,rightShoulder,leftShoulder,upperTorso,
+				midTorso,lowTorso,pelvis,rightHip,leftHip };
+
+			for (auto aimpoint : hitboxes)
+			{
+				vec2 playerScreenPosition;
+				//checks if w2s fails on bone or that this player is still a valid target
+				//sets playerscreenposition to current bone if succeed
+				if (!playerList[bestTarget].validPlayer() || !worldToScreen(playerList[bestTarget].bones[aimpoint], playerScreenPosition, getViewMatrix()))
+				{
+					//failed
+					continue;
+				}
+				const float distToCenter = playerScreenPosition.distance(screenCenter);
+				if (distToCenter < minDist)
+				{
+					minDist = distToCenter;
+					bestBone = aimpoint;
+				}
+			}
+		}
+		return bestBone;
+	}
+
+	
 	//shared helper functions for both aimbots
-	uint32_t chooseEnemy(float& distanceToCenter, bool friendlyFire)
+	auto chooseEnemy(float& distanceToCenter, bool friendlyFire) -> uint32_t
 	{
 		CSPlayer& localPlayer = playerList.getLocalPlayer();
 		uint32_t bestIndex;
-		vec2 screenCenter = vec2(globals.screenWidth / 2.f, globals.screenHeight / 2.f);
+		const vec2 screenCenter = vec2(globals.screenWidth / 2.f, globals.screenHeight / 2.f);
 		float minDist = std::numeric_limits<float>::max();
 		float dist = 0;
-		for (auto i = 0; i < playerList.size(); i++)
+		for (auto i = 0u; i < playerList.size(); i++)
 		{
 			vec2 playerScreenPosition;
+			const CSPlayer& currentPlayer = playerList[i];
 			//if failed to read adequate number of hitboxes
-			if (!playerList[i].hitboxReadSuccess)
+			if (!currentPlayer.hitboxReadSuccess)
 			{
 				continue;
 			}
 			//if trying to read your own shit, obviously not gonna aim at urself LOL!!! :D XD
-			if (playerList[i].base == localPlayer.base)
+			if (currentPlayer.base == localPlayer.base)
 			{
 				continue;
 			}
-			//skip teammates that cannot be aimed at if friendly fire is on
-			if (!friendlyFire && !playerList[i].validTarget)
+			//skip teammates that cannot be aimed at if friendly fire is off
+			if (!friendlyFire && !currentPlayer.validTarget(localPlayer))
 			{
 				continue;
 			}
 			//what the fuck is this?
 			//checks if w2s fails, sets playerscreenposition, lol what a shit
 			//bone 5 is middle torso. this may be changed in the future.
-			if (!playerList[i].validPlayer() || !worldToScreen(playerList[i].bones[5], playerScreenPosition, getViewMatrix()))
+			if (!currentPlayer.validPlayer() || !worldToScreen(currentPlayer.bones[5], playerScreenPosition, getViewMatrix()))
 			{
 				//did not succeed. if it did succeed, this block will skip and code will continue to execute using playerScreenPosition
 				continue;
 			}
 
-			float distToCenter = playerScreenPosition.distance(screenCenter);
+			const float distToCenter = playerScreenPosition.distance(screenCenter);
 			//update minDist if a closer player is found
 			if (distToCenter < minDist)
 			{
@@ -63,7 +114,13 @@ namespace valkyrie
 			}
 			dist = minDist;
 		}
+		//set reference var
 		distanceToCenter = dist;
 		return bestIndex;
+	}
+
+	auto aimAtEnemy(const vec3& enemyPos, const vec3& localPos, float enemyDistance) -> void
+	{
+		//tbd
 	}
 }
--- ValkyrieWork/aimbotfeatures.h
+++ ValkyrieWork/aimbotfeatures.h
@@ -1,1 +1,68 @@
 #pragma once
+
+#include "res.h"
+#include "feature.h"
+
+namespace valkyrie
+{
+	enum HitboxID
+	{
+		head = 0,
+		neck = 1,
+		rightShoulder = 17,
+		leftShoulder = 15,
+		upperTorso = 6,
+		midTorso = 5,
+		lowTorso = 4,
+		pelvis = 2,
+		rightHip = 8,
+		leftHip = 7,
+	};
+
+	class LegitBot : public Feature
+	{
+	private:
+		float aimStrength;
+		float aimFov;
+		float aimRandomizeAmt;
+		bool rcsEnabled = false;
+		bool friendlyFire = false;
+
+		auto chooseBone(const uint32_t bestTarget) const -> HitboxID;
+
+	public:
+		auto execFeature() const -> void;
+		auto getFeatureName() const -> string const& { return "Legitbot"; }
+
+		constexpr auto setAimStrength(uint32_t menuIndex) -> void;
+		constexpr auto setAimFov(uint32_t menuIndex) -> void;
+		constexpr auto setAimRandomization(uint32_t menuIndex) -> void;
+		constexpr auto setRcsMode(uint32_t menuIndex) -> void { rcsEnabled = menuIndex; }
+		constexpr auto setFFMode(uint32_t menuIndex) -> void { friendlyFire = menuIndex; }
+
+	};
+
+	class RageBot : public Feature
+	{
+	private:
+		const float aimStrength = 0.99999;
+		float aimFov;
+		float aimRandomizeAmt;
+		bool rcsEnabled = false;
+		bool friendlyFire = false;
+		bool bodyShots = false;
+
+		auto chooseBone(const uint32_t bestTarget) const->HitboxID;
+
+	public:
+		auto execFeature() const -> void;
+		auto getFeatureName() const -> string const& { return "Ragebot"; }
+
+		constexpr auto setAimFov(uint32_t menuIndex) -> void;
+		constexpr auto setAimRandomization(uint32_t menuIndex) -> void;
+		constexpr auto setRcsMode(uint32_t menuIndex) -> void { rcsEnabled = menuIndex; }
+		constexpr auto setFFMode(uint32_t menuIndex) -> void { friendlyFire = menuIndex; }
+		constexpr auto setBodyShotMode(uint32_t menuIndex) -> void { bodyShots = menuIndex; }
+
+	};
+}
